2946a54cbc96a38a6817b6aab9bbb1c0a89edd64
t2m1r <tamer_ioc@hotmail.com>
2023-02-16 16:15:48 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using printf function
+ * Return: 0 (Success)
+ */
 
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a price of art,\n");
+	return (0);
+}
