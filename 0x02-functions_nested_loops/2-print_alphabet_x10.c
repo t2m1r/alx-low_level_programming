10651ee69f3da34d564df2c5ff395077334d3e24
t2m1r <tamer_ioc@hotmail.com>
2023-02-21 16:47:10 -0800

    10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,10 @@ void print_alphabet_x10(void)
 	int ten;
 	char letter;
 
-	for (int ten = 0; ten <= 10; ten++)
+	for (ten = 0; ten < 10; ten++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-
-	_putchar('\n);
+	_putchar('\n');
 	}
 }
