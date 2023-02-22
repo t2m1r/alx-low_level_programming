2d14c80aa9fda1a562974d086e37aaacfe618c21
t2m1r <tamer_ioc@hotmail.com>
2023-02-21 16:17:55 -0800

    loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print alphabetes
+ */
+
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+
+	for (int i = 0; < 10; i++)
+	{
+		while (c <= 'z')
+	
+	{
+	_putchar(c);
+	}
+	c = 'a';
+	_putchar('\n);
+	}
+}
